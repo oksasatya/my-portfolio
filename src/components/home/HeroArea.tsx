e89ed6e40884a479f9e0827a0fd6069d6d7b7234
oksa satya <oksasatyaa@gmail.com>
2025-08-15 11:58:53 +0400

    add new image and redesain

--- src/components/home/HeroArea.tsx
+++ src/components/home/HeroArea.tsx
@@ -40,7 +40,7 @@ export default function HeroArea() {
             </div>
             <div className="col-lg-6">
               <div className="hero-image">
-                <img src="/assets/images/about/me.png" alt="" />
+                <img src="/assets/images/about/me.jpg" alt="" />
               </div>
 
             </div>
